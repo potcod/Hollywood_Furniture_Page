1ccf2f6b7ada81be5c59e797706c98898dfb2fff
potcod <justinta2201@gmail.com>
2025-05-27 15:47:44 -0700

    Added some comments

--- hollywood_furniture/src/App.js
+++ hollywood_furniture/src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
 
   return (
-    <div >
+    // Overall div for the app
+    <div> 
       
+      {/* Div for title and navigation bar  */}
       <div>
         <h1 className="h1"> Hollywood Furniture</h1>
 
@@ -14,6 +16,8 @@ function App() {
         </h2>
       </div>
 
+
+      {/* Div for main content, display slideshow of images, etc */}
       <div className="main">
 
 
