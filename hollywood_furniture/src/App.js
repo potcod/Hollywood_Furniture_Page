2b6ac762a7706d0fcd88ae1968fe9fc45bdb272e
potcod <justinta2201@gmail.com>
2025-05-27 15:45:42 -0700

    Fixed repo

---
+++ hollywood_furniture/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div >
+      
+      <div>
+        <h1 className="h1"> Hollywood Furniture</h1>
+
+        <h2 className="h2">
+          NavBar
+        </h2>
+      </div>
+
+      <div className="main">
+
+
+      </div>
+
+
+    </div>
+
+
+  );
+}
+
+export default App;
